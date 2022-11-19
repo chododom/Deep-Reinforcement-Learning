8a53c81a8951c2e1053ef806ce4671e42d9de8ba
Juraj Kmec <xjurajkmec@gmail.com>
2022-11-19 23:17:35 +0100

    PAAC passing solution.

--- Week07/paac/paac.py
+++ Week07/paac/paac.py
@@ -12,6 +12,7 @@
 
 import argparse
 import os
+
 os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
 
 import gym
@@ -27,34 +28,49 @@ parser.add_argument("--env", default="CartPole-v1", type=str, help="Environment.
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
 parser.add_argument("--seed", default=None, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--threads", default=0, type=int, help="Maximum number of threads to use.")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--entropy_regularization", default=None, type=float, help="Entropy regularization weight.")
-parser.add_argument("--envs", default=None, type=int, help="Number of parallel environments.")
-parser.add_argument("--evaluate_each", default=100, type=int, help="Evaluate each number of batches.")
-parser.add_argument("--evaluate_for", default=10, type=int, help="Evaluate the given number of episodes.")
-parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
-parser.add_argument("--hidden_layer_size", default=None, type=int, help="Size of hidden layer.")
-parser.add_argument("--learning_rate", default=None, type=float, help="Learning rate.")
+parser.add_argument("--envs", default=16, type=int, help="Number of parallel environments.")
+parser.add_argument("--evaluate_each", default=500, type=int, help="Evaluate each number of batches.")
+parser.add_argument("--evaluate_for", default=100, type=int, help="Evaluate the given number of episodes.")
+parser.add_argument("--gamma", default=0.99, type=float, help="Discounting factor.")
+parser.add_argument("--hidden_layer_size", default=64, type=int, help="Size of hidden layer.")
+parser.add_argument("--learning_rate", default=0.001, type=float, help="Learning rate.")
 
 
 class Network:
     def __init__(self, env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
-        # TODO: Similarly to reinforce with baseline, define two components:
+        # Similarly to reinforce with baseline, define two components:
         # - actor, which predicts distribution over the actions
         # - critic, which predicts the value function
         #
         # Use independent networks for both of them, each with
         # `args.hidden_layer_size` neurons in one ReLU hidden layer,
         # and train them using Adam with given `args.learning_rate`.
-        raise NotImplementedError()
+        inputs = tf.keras.Input(shape=env.observation_space.shape, name='Input')
+        x = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(inputs)
+        output = tf.keras.layers.Dense(env.action_space.n, activation='softmax')(x)
+        actor_network = tf.keras.Model(inputs=inputs, outputs=output)
+        actor_network.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                              optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate))
+
+        inputs = tf.keras.Input(shape=env.observation_space.shape, name='Input')
+        x = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(inputs)
+        output = tf.keras.layers.Dense(1)(x)
+        critic_network = tf.keras.Model(inputs=inputs, outputs=output)
+        critic_network.compile(loss=tf.keras.losses.MeanSquaredError(),
+                               optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate))
+
+        self._actor_network = actor_network
+        self._critic_network = critic_network
 
     # The `wrappers.typed_np_function` automatically converts input arguments
     # to NumPy arrays of given type, and converts the result to a NumPy array.
     @wrappers.typed_np_function(np.float32, np.int32, np.float32)
     @wrappers.raw_tf_function(dynamic_dims=1)
     def train(self, states: np.ndarray, actions: np.ndarray, returns: np.ndarray) -> None:
-        # TODO: Train the policy network using policy gradient theorem
+        # Train the policy network using policy gradient theorem
         # and the value network using MSE.
         #
         # The `args.entropy_regularization` might be used to include actor
@@ -62,19 +78,42 @@ class Network:
         # quite easily without it (my reference solution does not use it).
         # In any case, `tfp.distributions.Categorical` is the suitable distribution;
         # in PyTorch, it is `torch.distributions.Categorical`.
-        raise NotImplementedError()
+
+        # Train the actor network with advantages as sample weights.
+        advantages = returns - tf.squeeze(self._critic_network(states))
+        with tf.GradientTape() as actor_tape:
+            y_true = actions
+            y_pred = self._actor_network(states, training=True)
+            actor_loss = self._actor_network.loss(y_true, y_pred, sample_weight=advantages)
+
+        # Train the critic network to predict value function.
+        with tf.GradientTape() as critic_tape:
+            y_true = returns
+            y_pred = tf.squeeze(self._critic_network(states, training=True))
+            critic_loss = self._critic_network.loss(y_true, y_pred)
+
+        self._actor_network.optimizer.minimize(actor_loss,
+                                               self._actor_network.trainable_variables, tape=actor_tape)
+        self._critic_network.optimizer.minimize(critic_loss,
+                                               self._critic_network.trainable_variables, tape=critic_tape)
 
     @wrappers.typed_np_function(np.float32)
     @wrappers.raw_tf_function(dynamic_dims=1)
     def predict_actions(self, states: np.ndarray) -> np.ndarray:
-        # TODO: Return predicted action probabilities.
-        raise NotImplementedError()
+        # Return predicted action probabilities.
+        return self._actor_network(states)
+
+    @wrappers.typed_np_function(np.float32)
+    def sample_actions(self, probs: np.ndarray) -> np.ndarray:
+        # Sample actions according to predicted action probabilities.
+        actions = tf.random.categorical(np.log(probs), num_samples=1)
+        return tf.squeeze(actions)
 
     @wrappers.typed_np_function(np.float32)
     @wrappers.raw_tf_function(dynamic_dims=1)
     def predict_values(self, states: np.ndarray) -> np.ndarray:
-        # TODO: Return estimates of value function.
-        raise NotImplementedError()
+        # Return estimates of value function.
+        return tf.squeeze(self._critic_network(states))
 
 
 def main(env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
@@ -90,8 +129,8 @@ def main(env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
     def evaluate_episode(start_evaluation: bool = False, logging: bool = True) -> float:
         rewards, state, done = 0, env.reset(start_evaluation=start_evaluation, logging=logging)[0], False
         while not done:
-            # TODO: Predict the action using the greedy policy.
-            action = ...
+            # Predict the action using the greedy policy.
+            action = np.argmax(network.predict_actions([state]))
             state, reward, terminated, truncated, _ = env.step(action)
             done = terminated or truncated
             rewards += reward
@@ -99,27 +138,37 @@ def main(env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
 
     # Create the vectorized environment
     vector_env = gym.vector.make(args.env, args.envs, asynchronous=True)
-    states = vector_env.reset(seed=args.seed)[0]
+    try:
+        states = vector_env.reset(seed=args.seed)[0]
+        training = True
+        while training:
+            # Training
+            for _ in range(args.evaluate_each):
+                # Choose actions using `network.predict_actions`.
+                probs = network.predict_actions(states)
+                actions = network.sample_actions(probs)
 
-    training = True
-    while training:
-        # Training
-        for _ in range(args.evaluate_each):
-            # TODO: Choose actions using `network.predict_actions`.
-            actions = ...
+                # Perform steps in the vectorized environment
+                next_states, rewards, terminated, truncated, _ = vector_env.step(actions)
+                dones = terminated | truncated
 
-            # Perform steps in the vectorized environment
-            next_states, rewards, terminated, truncated, _ = vector_env.step(actions)
-            dones = terminated | truncated
+                # Compute estimates of returns by one-step bootstrapping
+                estimated_returns = rewards + args.gamma * (1 - dones) * network.predict_values(next_states)
 
-            # TODO: Compute estimates of returns by one-step bootstrapping
+                # Train network using current states, chosen actions and estimated returns
+                network.train(states, actions, estimated_returns)
 
-            # TODO: Train network using current states, chosen actions and estimated returns
+                states = next_states
 
-            states = next_states
-
-        # Periodic evaluation
-        returns = [evaluate_episode() for _ in range(args.evaluate_for)]
+            # Periodic evaluation
+            returns = [evaluate_episode(logging=False) for _ in range(args.evaluate_for)]
+            # print(np.mean(returns), '+-', np.std(returns))
+            if np.mean(returns) - np.std(returns) > 450:
+                training = False
+    except KeyboardInterrupt:
+        pass
+    finally:
+        vector_env.close()
 
     # Final evaluation
     while True:
